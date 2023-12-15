e701fbe0b2c49ffbed7647fdc703e44b5cf903b0
prasetyahs <prasetya2423@gmail.com>
2023-12-15 23:46:26 +0700

    fix database seeder

--- database/seeders/TravelSeeder.php
+++ database/seeders/TravelSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\Travel;
 
@@ -13,20 +14,21 @@ class TravelSeeder extends Seeder
      * @return void
      */
     public function run()
-    {   
+    {
         $jsonFilePath = storage_path('app\tourism_with_id.json');
         $jsonString = file_get_contents($jsonFilePath);
         $dataArray = json_decode($jsonString, true);
 
-        foreach($dataArray as $d){
+        foreach ($dataArray as $d) {
             $travel = new Travel();
+            $category = Category::where("nama", $d['Category'])->first();
             $travel->name = $d['Place_Name'];
             $travel->description = $d['Description'];
             $travel->city = $d['City'];
             $travel->lat = $d['Lat'];
             $travel->lon = $d['Long'];
             $travel->price = $d['Price'];
-            $travel->category = 41;
+            $travel->category =  $category->id;
             $travel->save();
         }
     }
